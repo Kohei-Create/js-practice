bc613b42e6d46baf1512a4122d5ff96bbcb784a3
Kohei Yoshida <was914hs@gmail.com>
2026-04-15 18:31:00 +0900

    StringとgetMonthとgetDateが足りていなかったのを修正

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,9 @@
 const timeElement = document.getElementById('current-year');
 const now = new Date();
 
-const year = (now.getFullYear());
-const month = (now.getFullYear() +1).padStart(2, '0');
-const date = (now.getFullYear()).padStart(2, '0');
+const year = now.getFullYear();
+const month = String(now.getMonth() +1).padStart(2, '0');
+const date = String(now.getDate()).padStart(2, '0');
 
 //<time>タグの中身を書き換える。
 timeElement.textContent = `${year}年${month}月${date}日`;
