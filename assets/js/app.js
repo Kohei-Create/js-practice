255c2d0c01f9bad793bab21201804b340b9cdff6
Kohei Yoshida <was914hs@gmail.com>
2026-04-15 17:52:03 +0900

    年月日を取得

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+const timeElement = document.getElementById('current-year');
+const now = new Date();
+
+const year = now.getFullYear();
+const month = (now.getFullYear() +1).padStart(2, '0');
+const date = (now.getFullYear()).padStart(2, '0');
+
+//<time>タグの中身を書き換える。
+timeElement.textContent = `${year}年${month}月${date}日`;
+
+//<time datetime="YYYY-CC-DD">
+timeElement.setAttribute('datetime', `${year}-${month}-${date}`);
+
